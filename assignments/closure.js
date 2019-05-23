04383c16156f00466e603367cb54f7be03786df7
Carl Stanley <49922081+Carl-Stanley@users.noreply.github.com>
2019-05-22 19:59:03 -0700

    Commiting Changes to Closure.

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,17 +1,42 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+const adapt = 'change';
+
+function evolve() {
+
+  return adapt; 
+
+}
+
+console.log(evolve());
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
+
+  
+
+  let count =0;
+// Return a function that when invoked increments and returns a counter variable.
+  return function() {
+    count = count + 1;
+    return count;
+  }
+
 };
+
 // Example usage: const newCounter = counter();
+const newCounter = counter();
+
 // newCounter(); // 1
 // newCounter(); // 2
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
+
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
